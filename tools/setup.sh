e463216f82f28c63cfccabd8080c9fbfc3d1b2d5
kelleyrw <kelleyrw@gmail.com>
2014-12-31 13:10:15 -0800

    fixed setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# get the project dir
+tools_dir=$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+project_dir=$(cd -P $tools_dir/.. && pwd)
+
 # setup external libs
-pushd external_libs
+pushd ${project_dir}/external_libs
 
 echo -e "\nInstalling PyUblas..."
 ./setup_pyublas.sh
