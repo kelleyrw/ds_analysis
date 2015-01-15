ad6f626352301eba68b7a7f8acbf506ddf53fe19
kelleyrw <kelleyrw@gmail.com>
2015-01-14 17:32:41 -0800

    pushed example

--- libs/boost_python_example/python/dict_example.py
+++ libs/boost_python_example/python/dict_example.py
@@ -1,5 +1,12 @@
 import dict_example as de
 print(dir(de))
 
-my_dict = {'pythondict': 1, 'foo': 'bar', 'Bach': 'Goldberg Variation' }
-de.pass_dict(my_dict)
+# my_dict = {'pythondict': 1, 'foo': 'bar', 'Bach': 'Goldberg Variation' }
+# de.pass_dict(my_dict)
+
+import numpy as np
+a1 = np.array([1,2,3])
+a2 = np.array([4,5,6,7])
+a3 = np.array([[9,10,11,13],[14,15,16,17]])
+d = {'one': a1, 'two' : a2, 'three': a3}
+de.pass_dict_np(d)
--- libs/boost_python_example/source/dict_example.cpp
+++ libs/boost_python_example/source/dict_example.cpp
@@ -1,16 +1,18 @@
+#include <iostream>
 #include <boost/python.hpp>
 #include <boost/python/object.hpp>
 #include <boost/python/extract.hpp>
 #include <boost/python/list.hpp>
 #include <boost/python/dict.hpp>
-#include <iostream>
+#include <boost/numpy.hpp>
+#include <boost/scoped_array.hpp>
 
 
-/// using namespace only for example
+// using namespace only for example
 using namespace boost::python;
+namespace np = boost::numpy;
 
-/// pass a python object, but this should be a python dictionary.
-/// \param[in] pydict a dictionary
+// pass a python object, but this should be a python dictionary.
 void pass_dict(boost::python::object const &pydict)
 {
     boost::python::extract<boost::python::dict> cppdict_ext(pydict);
@@ -34,8 +36,35 @@ void pass_dict(boost::python::object const &pydict)
     }
 }
 
+// pass a python object, but this should be a python dictionary of numpy values.
+void pass_dict_np(boost::python::object const &pydict)
+{
+    boost::python::extract<boost::python::dict> cppdict_ext(pydict);
+    if(!cppdict_ext.check())
+    {
+        throw std::runtime_error("PassObj::pass_dict: type error: not a python dict.");
+    }
+
+    boost::python::dict const cppdict = cppdict_ext();
+    boost::python::list const keylist = cppdict.keys();
+
+    // careful with boost name. there already have a conflict.
+    int const len = boost::python::len(keylist);
+    std::cout << "len(keylist) = " << len << std::endl;
+    for(int i = 0; i < len; ++i)
+    {
+        // operator[] is in python::boost::object
+        std::string const key_str = extract<std::string>(str(keylist[i]     ));
+        np::ndarray const val_array = extract<np::ndarray>(cppdict[keylist[i]]);
+//         std::cout << "key:[" << key_str << "]->[" << val_array.shape(0) << "]" << std::endl;
+//         std::cout << "key:[" << key_str << "]->[" << val_array.shape(0) << "]" << std::endl;
+    }
+}
+
+
 BOOST_PYTHON_MODULE(dict_example)
 {
 //     np::initialize();  // have to put this in any module that uses Boost.NumPy
-    boost::python::def("pass_dict", pass_dict);
+    boost::python::def("pass_dict"   , pass_dict);
+    boost::python::def("pass_dict_np", pass_dict_np);
 }
