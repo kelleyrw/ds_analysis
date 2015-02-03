4fcdef71c9b588c77c4924250fe91d365f60b3fd
kelleyrw <kelleyrw@gmail.com>
2015-02-02 17:41:08 -0800

    sync

--- libs/boost_python_example/source/dict_example.cpp
+++ libs/boost_python_example/source/dict_example.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <map>
 #include <boost/python.hpp>
 #include <boost/python/object.hpp>
 #include <boost/python/extract.hpp>
@@ -6,7 +8,7 @@
 #include <boost/python/dict.hpp>
 #include <boost/numpy.hpp>
 #include <boost/scoped_array.hpp>
-
+#include "NumPyArrayData.hpp"  // need to put this somewhere resuable
 
 // using namespace only for example
 using namespace boost::python;
@@ -36,6 +38,22 @@ void pass_dict(boost::python::object const &pydict)
     }
 }
 
+// a candidate for "resuable" tools
+std::string Array1DString(np::ndarray const &array)
+{
+    int const N = array.shape(0);
+    NumPyArrayData<int> array_data(array);
+    std::stringstream os;
+    os << "{";
+    for (int i = 0; i < N; ++i)
+    {
+        os << array_data(i);
+        if (i != (N - 1)) os << ", ";
+    }
+    os << "}";
+    return os.str();
+}
+
 // pass a python object, but this should be a python dictionary of numpy values.
 void pass_dict_np(boost::python::object const &pydict)
 {
@@ -56,7 +74,24 @@ void pass_dict_np(boost::python::object const &pydict)
         // operator[] is in python::boost::object
         std::string const key_str   = extract<std::string>(str(keylist[i]     ));
         np::ndarray const val_array = extract<np::ndarray>(cppdict[keylist[i]]);
-        std::cout << "key:[" << key_str << "]->[" << val_array.shape(0) << "]" << std::endl;
+        NumPyArrayData<int> val_array_data(val_array);
+        std::cout << "key:[" << key_str << "] = " << Array1DString(val_array) << "}" << std::endl;
+    }
+
+    std::map<std::string, np::ndarray> m;
+    for(int i = 0; i < len; ++i)
+    {
+        // operator[] is in python::boost::object
+        std::string const key   = extract<std::string>(str(keylist[i]     ));
+        np::ndarray const value = extract<np::ndarray>(cppdict[keylist[i]]);
+        m[key] = value;
+    }
+
+    for (auto iter = m.begin(); iter != m.end(); iter++)
+    {
+        auto key   = iter->first;
+        auto value = iter->second;
+        std::cout << "m[" << key << "] = " << Array1DString(value) << std::endl;
     }
 }
 
